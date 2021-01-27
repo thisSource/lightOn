a51c2e26f5e11d5698055a18dcdc2a63f4a0e6e5
alxSource <alexander.hoglund@gmail.com>
2021-01-27 23:04:00 +0100

    first to github

--- public/sketch.js
+++ public/sketch.js
@@ -1,91 +1,81 @@
+const button = document.getElementById("button");
+const onCss = document.getElementById("on");
+const offCss = document.getElementById("off");
 
-const button = document.getElementById("button")
-const onCss = document.getElementById("on")
-const offCss = document.getElementById("off")
-
-const background = document.querySelector("body")
+const background = document.querySelector("body");
 let dataF;
-let nodeAPI = "/api"
-
+let nodeAPI = "/api";
 let socket = io();
-runSystemFromStart ()
-async function runSystemFromStart (){
-
-let dataStart = await fetch(nodeAPI)
-let dataToUse = await dataStart.json()
-
-let onOff = dataToUse;
-if(onOff === true){
-    button.style.background ="lightgray"
-    onCss.style.color = "green"
-    offCss.style.color = "darkgray"
-    background.style.background ="white"
-} else {
-    button.style.background ="gray"
-        onCss.style.color = "darkgray"
-        offCss.style.color = "red"
-        background.style.background ="black"
-}
 
 
-console.log(dataToUse)
+runSystemFromStart();
+async function runSystemFromStart() {
+  let dataStart = await fetch(nodeAPI);
+  let dataToUse = await dataStart.json();
+//   
 
-socket.on("click", (data)=>{
-    if(data.isOn === true){
-        button.style.background ="lightgray"
-        onCss.style.color = "green"
-        offCss.style.color = "darkgray"
-        background.style.background ="white"
-        clickOnSound()
+  let onOff = dataToUse;
+  if (onOff === true) {
+    button.style.background = "lightgray";
+    onCss.style.color = "green";
+    offCss.style.color = "darkgray";
+    background.style.background = "white";
+  } else {
+    button.style.background = "gray";
+    onCss.style.color = "darkgray";
+    offCss.style.color = "red";
+    background.style.background = "black";
+  }
 
+  console.log(dataToUse);
+
+  socket.on("click", (data) => {
+    if (data.isOn === true) {
+      button.style.background = "lightgray";
+      onCss.style.color = "green";
+      offCss.style.color = "darkgray";
+      background.style.background = "white";
+      clickOnSound();
     } else {
-        button.style.background ="gray"
-        onCss.style.color = "darkgray"
-        offCss.style.color = "red"
-        background.style.background ="black"
-        clickOffSound()
+      button.style.background = "gray";
+      onCss.style.color = "darkgray";
+      offCss.style.color = "red";
+      background.style.background = "black";
+      clickOffSound();
     }
-})
+  });
 
+  button.addEventListener("click", () => {
+    if (onOff.isOn === false) {
+      onOff = { isOn: true };
 
+      button.style.background = "lightgray";
+      onCss.style.color = "green";
+      offCss.style.color = "darkgray";
+      background.style.background = "white";
+      clickOnSound();
+    } else {
+      onOff = { isOn: false };
 
+      button.style.background = "gray";
+      onCss.style.color = "darkgray";
+      offCss.style.color = "red";
+      background.style.background = "black";
+      clickOffSound();
+    }
 
+    socket.emit("click", onOff);
 
+    // console.log("sending... " + onOff)
+  });
 
-button.addEventListener("click", ()=>{
-if (onOff.isOn === false){
-    onOff = {isOn: true}
-   
-    button.style.background ="lightgray"
-    onCss.style.color = "green"
-    offCss.style.color = "darkgray"
-    background.style.background ="white"
-    clickOnSound()
-} else {
-    onOff = {isOn: false}
-   
-    button.style.background ="gray"
-    onCss.style.color = "darkgray"
-    offCss.style.color = "red"
-    background.style.background ="black"
-    clickOffSound()
-}
-
-
-socket.emit("click", onOff)
-
-// console.log("sending... " + onOff)
-})
- 
-function clickOnSound(){
+  function clickOnSound() {
     var audio = new Audio("Click.mp3");
     audio.play();
   }
-  
-  function clickOffSound(){
+
+  function clickOffSound() {
     var audio = new Audio("ClickFast.mp3");
     audio.play();
   }
-
-
 }
--- server.js
+++ server.js
@@ -1,25 +1,20 @@
 const express = require("express");
 const socket = require("socket.io");
-const Datastore = require("nedb")
+const Datastore = require("nedb");
 const app = express();
-
 
 app.use(express.static("public"));
 
 app.get("/api", (req, res) => {
-  let startState
+  let startState;
   db.find({}, function (err, docs) {
-    startState = docs[0].isOn
+    startState = docs[0].isOn;
     res.status(200).send(startState);
-
   });
 });
 
-const db = new Datastore({filename:"database.db", autoload: true});
+const db = new Datastore({ filename: "database.db", autoload: true });
 db.loadDatabase();
-
-
-
 
 const server = app.listen(process.env.PORT || 3000, () => {
   console.log("listening to port 3000...");
@@ -30,15 +25,19 @@ io.sockets.on("connection", (socket) => {
   console.log("new connection " + socket.id);
 
   socket.on("click", (data) => {
-    db.update({}, { $set: { isOn: data.isOn } }, { multi: true }, function (err, numReplaced) {
-  // numReplaced = 3
-  // Field 'system' on Mars, Earth, Jupiter now has value 'solar system'
-});
-    db.insert(data)
-      socket.broadcast.emit("click", data)
+    db.update(
+      {},
+      { $set: { isOn: data.isOn } },
+      { multi: false },
+      function (err, numReplaced) {
+        numReplaced = 0;
+      }
+    );
+    //for repair
+    // db.insert(data)
+    //{"isOn":true,"_id":"ZXggbluAKPop9CIu"}
+
+    socket.broadcast.emit("click", data);
     console.log(data);
-
   });
-
- 
 });
